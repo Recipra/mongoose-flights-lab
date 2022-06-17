b6c53d2f070954d09fb73be614e30fae2b281e6b
recipra <jmo0425@outlook.com>
2022-06-17 12:03:54 -0400

    add controllers and import to routes

---
+++ controllers/flights.js
@@ -0,0 +1,35 @@
+import { Flight } from "../models/flight.js"
+
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function newFlight(req, res) {
+  res.render('flights/new')
+}
+
+function create(req, res) {
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights')
+  })
+}
+
+export {
+  index,
+  newFlight as new,
+  create,
+}
